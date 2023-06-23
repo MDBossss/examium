4efbd9a183efe69d7f6d02cca26e971de60a064c
MDBossss <ferrari020203@gmail.com>
2023-06-23 15:16:17 +0200

    test

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -7,7 +7,6 @@ import { Button } from "../components/ui/button";
 const Create = () => {
 	const [testTitle, setTestTitle] = useState<string>("");
 
-
 	return (
 		<div className="flex flex-col gap-10 p-10 w-full">
 			<SearchBar />
@@ -18,8 +17,12 @@ const Create = () => {
 					have at least one correct answer.
 				</p>
 				<div className="flex gap-5 mb-2 p-2">
-					<Input placeholder="Insert test name..." onChange={(e) => setTestTitle(e.target.value)} className="bg-slate-200" />
-          <Button className="bg-green-500 hover:bg-green-600">Save</Button>
+					<Input
+						placeholder="Insert test name..."
+						onChange={(e) => setTestTitle(e.target.value)}
+						className="bg-slate-200"
+					/>
+					<Button className="bg-green-500 hover:bg-green-600">Save</Button>
 				</div>
 			</div>
 			<Question />
--- src/types/models.ts
+++ src/types/models.ts
@@ -1,4 +1,10 @@
-export interface QuestionModel{
+export interface QuestionType{
     question: string,
     
+}
+
+export interface AnswerType{
+    answer:string,
+    isCorrect:boolean,
+    image?:string
 }
