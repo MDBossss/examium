8cfa46b5216788c647cb6ffb729915646f2b48bd
MDBossss <ferrari020203@gmail.com>
2023-09-30 21:21:24 +0200

    init

--- client/src/pages/Schedule.tsx
+++ client/src/pages/Schedule.tsx
@@ -1,8 +1,85 @@
+import { Scheduler } from "@aldabil/react-scheduler";
+import { SchedulerRef } from "@aldabil/react-scheduler/types";
+import { useMemo, useRef, useState } from "react";
+import { useThemeStore } from "../store/themeStore";
+import SchedulerEditor from "../components/SchedulerEditor";
+import SearchBar from "../components/SearchBar";
 
 const Schedule = () => {
-  return (
-    <div>Schedule</div>
-  )
-}
+	const { theme } = useThemeStore();
+	const [date, setDate] = useState<Date>(useMemo(() => new Date(), [])); // fetch to here? or use react query
+	const schedulerRef = useRef<SchedulerRef>(null);
 
-export default Schedule
+	return (
+		<div className="flex flex-col w-full gap-10 p-4 pt-5 max-w-screen sm:p-10">
+			<SearchBar/>
+			<div className={`${theme === "dark" ? "darkmode-scheduler" : "lightmode-scheduler"}`}>
+				<Scheduler
+					ref={schedulerRef}
+					customEditor={(scheduler) => (
+						<SchedulerEditor scheduler={scheduler} schedulerRef={schedulerRef} />
+					)}
+					viewerExtraComponent={(fields, event) => {
+						return <p>{event.description}</p>;
+					}}
+					view="week"
+					fields={[{ name: "recurrencePattern", type: "select" }]}
+					month={{
+						weekDays: [0, 1, 2, 3, 4, 5, 6],
+						weekStartOn: 1,
+						startHour: 8,
+						endHour: 21,
+						navigation: true,
+						disableGoToDay: false,
+					}}
+					week={{
+						weekDays: [0, 1, 2, 3, 4, 5, 6],
+						weekStartOn: 1,
+						startHour: 8,
+						endHour: 21,
+						step: 30,
+						navigation: true,
+						disableGoToDay: false,
+					}}
+					day={{
+						startHour: 8,
+						endHour: 21,
+						step: 30,
+						navigation: true,
+					}}
+					draggable={false}
+					editable={true}
+					deletable={true}
+					// locale={hr}
+					hourFormat="24"
+					events={[
+						{
+							event_id: 1,
+							title: "Event 1",
+							start: new Date("2023-09-25 09:30:00"),
+							end: new Date("2023-09-25 11:30:00"),
+							color: "#1d4ed8",
+						},
+						{
+							event_id: 2,
+							title: "Event 2",
+							start: new Date("2023-09-26 09:30:00"),
+							end: new Date("2023-09-26 10:30:00"),
+							color: "#1d4ed8",
+						},
+						{
+							event_id: 3,
+							title: "Dan za kupanje",
+							start: new Date("2023-09-27 09:30:00"),
+							end: new Date("2023-09-27 10:30:00"),
+							allDay: true,
+							color: "green",
+						},
+					]}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Schedule;
--- client/src/utils/dateUtils.ts
+++ client/src/utils/dateUtils.ts
@@ -1,26 +1,122 @@
+import { ProcessedEvent } from "@aldabil/react-scheduler/types";
+import {
+	addDays,
+	addMonths,
+	addWeeks,
+	getMonth,
+	getWeek,
+	getYear,
+	isBefore,
+	setHours,
+	setMinutes,
+} from "date-fns";
+
 export function getTimeAgo(timestamp: Date) {
-    const currentDate = new Date();
-    const previousDate = new Date(timestamp);
-    const timeDifference = currentDate.getTime() - previousDate.getTime();
-    const seconds = Math.floor(timeDifference / 1000);
-  
-    if (seconds < 60) {
-      return `${seconds} seconds ago`;
-    }
-  
-    const minutes = Math.floor(seconds / 60);
-  
-    if (minutes < 60) {
-      return `${minutes} minutes ago`;
-    }
-  
-    const hours = Math.floor(minutes / 60);
-  
-    if (hours < 24) {
-      return `${hours} hours ago`;
-    }
-  
-    const days = Math.floor(hours / 24);
-    return `${days} days ago`;
-  }
-  
+	const currentDate = new Date();
+	const previousDate = new Date(timestamp);
+	const timeDifference = currentDate.getTime() - previousDate.getTime();
+	const seconds = Math.floor(timeDifference / 1000);
+
+	if (seconds < 60) {
+		return `${seconds} seconds ago`;
+	}
+
+	const minutes = Math.floor(seconds / 60);
+
+	if (minutes < 60) {
+		return `${minutes} minutes ago`;
+	}
+
+	const hours = Math.floor(minutes / 60);
+
+	if (hours < 24) {
+		return `${hours} hours ago`;
+	}
+
+	const days = Math.floor(hours / 24);
+	return `${days} days ago`;
+}
+
+interface EditorInput {
+	event_id: string | number;
+	title: string;
+	description: string;
+	start: Date | string;
+	end: Date | string;
+	allDay: boolean | undefined;
+	color: string;
+	repeatPattern: "none" | "daily" | "weekly" | "monthly";
+}
+
+export function generateRepeatingEvents(event: EditorInput): ProcessedEvent[] {
+	const { start, end, repeatPattern } = event;
+	const occurrences: EditorInput[] = [];
+	let currentDate = new Date(start);
+
+	switch (repeatPattern) {
+		case "none":
+			occurrences.push(event);
+			return occurrences as ProcessedEvent[];
+
+		case "daily":
+			//Skips occurrences already passed
+			while (getWeek(currentDate, { weekStartsOn: 1 }) < getWeek(new Date(), { weekStartsOn: 1 })) {
+				currentDate = addDays(currentDate, 1);
+			}
+
+			while (isBefore(currentDate, addDays(new Date(start), 14))) {
+				occurrences.push({
+					...event,
+					start: new Date(currentDate),
+					end: new Date(
+						setHours(setMinutes(currentDate, new Date(end).getMinutes()), new Date(end).getHours())
+					),
+				});
+				currentDate = addDays(currentDate, 1);
+			}
+			console.log(occurrences);
+			return occurrences as ProcessedEvent[];
+
+		case "weekly":
+			//Skips occurrences already passed
+			while (getMonth(currentDate) < getMonth(new Date())) {
+				currentDate = addWeeks(currentDate, 1);
+			}
+
+			while (isBefore(currentDate, addWeeks(new Date(start), 14))) {
+				occurrences.push({
+					...event,
+					start: new Date(currentDate),
+					end: new Date(
+						setHours(setMinutes(currentDate, new Date(end).getMinutes()), new Date(end).getHours())
+					),
+				});
+				currentDate = addWeeks(currentDate, 1);
+			}
+			console.log(occurrences);
+			return occurrences as ProcessedEvent[];
+
+		case "monthly":
+			//Skips occurrences already passed
+			while (getYear(currentDate) < getYear(new Date())) {
+				currentDate = addMonths(currentDate, 1);
+			}
+
+			while (isBefore(currentDate, addMonths(new Date(start), 14))) {
+				occurrences.push({
+					...event,
+					start: new Date(currentDate),
+					end: new Date(
+						setHours(setMinutes(currentDate, new Date(end).getMinutes()), new Date(end).getHours())
+					),
+				});
+				currentDate = addMonths(currentDate, 1);
+			}
+			console.log(occurrences);
+			return occurrences as ProcessedEvent[];
+
+		default:
+			occurrences.push(event);
+			return occurrences as ProcessedEvent[];
+	}
+}
